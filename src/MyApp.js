eb62f356c604a5cba044506dfbd50b9b55abb6a0
Hannah Tobiason <hannahtobiason@Hannahs-MacBook-Pro-2.local>
2022-01-07 13:42:39 -0800

    created the submit button to update the form in Form.js

--- src/MyApp.js
+++ src/MyApp.js
@@ -8,7 +8,7 @@ function MyApp() {
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter = {removeOneCharacter} />
-        <Form />
+        <Form handleSubmit = {updateList}/>
       </div>
     );  
   
@@ -17,6 +17,10 @@ function MyApp() {
       return i !== index
     });
     setCharacters(updated);
+  }
+
+  function updateList(person){
+    setCharacters([...characters, person]);
   }
 }
 
