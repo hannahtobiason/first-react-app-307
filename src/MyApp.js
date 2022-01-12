f6649a45b14a30a6e0fdb26bc0f4a24c191115aa
Hannah Tobiason <hanbtob@gmail.com>
2022-01-11 18:44:54 -0800

    added axios to frontend to make requests through backend. commiting to branch

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios'
+import React, {uesState, useEffect} from 'react'
 
 
 function MyApp() {
@@ -10,7 +12,19 @@ function MyApp() {
         <Table characterData={characters} removeCharacter = {removeOneCharacter} />
         <Form handleSubmit = {updateList}/>
       </div>
-    );  
+    );
+    
+  async function fetchAll(){
+    try{
+      const response = await axios.get('http://localhost:5000/users')
+      return response.data.users_list;
+    }
+    catch (error){
+      //we're not handling errors. just logging into the console
+      console.log(error);
+      return false;
+    }
+  }
   
   function removeOneCharacter (index){
     const updated = characters.filter((character, i) => {
@@ -23,6 +37,14 @@ function MyApp() {
     setCharacters([...characters, person]);
   }
 }
+
+useEffect(() => {
+  fetchAll().then( result => {
+    if (result){
+      setCharacters(result);
+    }
+  });
+}, []);
 
 
 export default MyApp
